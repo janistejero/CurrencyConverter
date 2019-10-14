439d7cf78058a76805b0c9ce2a7a3f49ebcfc256
janis.tejero <janis.tejero@stud.bbbaden.ch>
2019-10-14 17:47:35 +0100

    Major Update
    - new UI
    - Functionality
    - Rounded Exchange rate
    - Logo

--- CurrencyConverter/src/currencyconverter/CalculatorController.java
+++ CurrencyConverter/src/currencyconverter/CalculatorController.java
@@ -1,20 +1,31 @@
 package currencyconverter;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 /**
  *
@@ -37,34 +48,68 @@ public class CalculatorController implements Initializable {
     @FXML
     private TextField amountTxt;
 
-    private CurrencyAPIParser currencyParser;
-
     private double convertedAmount = 0.00;
 
     private final ObservableList<String> options
             = FXCollections.observableArrayList(
-                    "AED", "ARS", "AUD", "BGN", "BRL", "BSD",
+                    "AED", "ARS", "AUD",
+                    "BGN", "BRL", "BSD",
                     "CAD", "CHF", "CLP", "CNY", "COP", "CZK",
-                    "DKK", "DOP", "EGP", "EUR", "FJD", "GBP",
-                    "GTQ", "HKD", "HRK", "HUF", "IDR", "ILS",
-                    "INR", "ISK", "JPY", "KRW", "KZT", "MXN",
-                    "MYR", "NOK", "NZD", "PAB", "PEN", "PHP",
-                    "PKR", "PLN", "PYG", "RON", "RUB", "SAR",
-                    "SEK", "SGD", "THB", "TRY", "TWD", "UAH", 
-                    "UYU", "VND", "ZAR"
+                    "DKK", "DOP" ,
+                    "EGP", "EUR",
+                    "FJD",
+                    "GBP",
+                    "GTQ",
+                    "HKD", "HRK", "HUF",
+                    "IDR", "ILS", "INR", "ISK",
+                    "JPY",
+                    "KRW",
+                    "KZT",
+                    "MXN", "MYR",
+                    "NOK", "NZD",
+                    "PAB", "PEN", "PHP", "PKR", "PLN", "PYG",
+                    "RON", "RUB",
+                    "SAR", "SEK", "SGD",
+                    "THB", "TRY", "TWD",
+                    "UAH", "USD", "UYU", 
+                    "VND",
+                    "ZAR"
             );
     @FXML
-    private Label amountLbl;
+    private Pane resultPane;
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         fromLbl.setText("");
         placeholderLbl.setText("");
-        amountLbl.setText("");
         toLbl.setText("");
 
-        fromCurrencyCmb.setItems(options);
-        toCurrencyCmb.setItems(options);
+        fromCurrencyCmb.getItems().addAll(options);
+        toCurrencyCmb.getItems().addAll(options);
+
+        fromCurrencyCmb.setOnAction(new  EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                fromLbl.setText(fromCurrencyCmb.getSelectionModel().getSelectedItem());
+            }
+        });
+
+        toCurrencyCmb.setOnAction(new  EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                toLbl.setText("in " + toCurrencyCmb.getSelectionModel().getSelectedItem());
+            }
+        });
+        
+        amountTxt.textProperty().addListener(new ChangeListener<String>(){
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                fromLbl.setText(newValue + " " + fromCurrencyCmb.getSelectionModel().getSelectedItem());
+           
+            }
+            
+        });
     }
 
     @FXML
@@ -75,14 +120,20 @@ public class CalculatorController implements Initializable {
         String to = toCurrencyCmb.getSelectionModel().getSelectedItem();
         String amount = amountTxt.getText();
 
+        fromCurrencyCmb.setPromptText(from);
+        toCurrencyCmb.setPromptText(to);
+
+        // simple validation
+        if (amount == null) {
+            placeholderLbl.setText("Please enter the amount");
+            valid = false;
+        }
+
         if (from == null) {
             placeholderLbl.setText("Please choose a currency from");
             valid = false;
         } else if (to == null) {
             placeholderLbl.setText("Please choose a currency to");
-            valid = false;
-        } else if (amount == null) {
-            placeholderLbl.setText("Please enter the amount");
             valid = false;
         } else {
             valid = true;
@@ -94,21 +145,44 @@ public class CalculatorController implements Initializable {
             // Setting URL to get rate from API
             String url_str = "https://api.exchangerate-api.com/v4/latest/" + from;
 
-            try {
-                exchangeRate = currencyParser.getExchangeRate(url_str, amount, to);
-            } catch (IOException ex) {
-                Logger.getLogger(CalculatorController.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            exchangeRate = getExchangeRate(url_str, to);
 
             BigDecimal rate = new BigDecimal(exchangeRate);
             BigDecimal amountBD = new BigDecimal(amount);
             BigDecimal result = amountBD.multiply(rate);
-
+            MathContext m = new MathContext(3);
+            result = result.round(m);
+            
+            
+            resultPane.getStyleClass().add("pane-border");
             fromLbl.setText(amountTxt.getText() + " " + from);
             placeholderLbl.setText("converts to");
-            amountLbl.setText(result.toString());
-            toLbl.setText(toCurrencyCmb.getSelectionModel().getSelectedItem());
+            toLbl.setText(result.toString() + " " + toCurrencyCmb.getSelectionModel().getSelectedItem());
         }
+    }
+
+    public String getExchangeRate(String url_str, String to) {
+        String exchangeRate = "";
+        try {
+            // Making Request
+            URL url = new URL(url_str);
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            // Convert to JSON
+            JsonParser jp = new JsonParser();
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonObject jsonobj = root.getAsJsonObject();
+
+            // Accessing object (com.google.gson.JsonObject)
+            JsonObject rates = jsonobj.getAsJsonObject("rates");
+
+            exchangeRate = rates.get(to).getAsString();
+        } catch (IOException ex) {
+            System.out.println("IOException");
+            placeholderLbl.setText("Error in converting occured");
+        }
+        return exchangeRate;
     }
 
 }
--- CurrencyConverter/src/currencyconverter/CurrencyConverter.java
+++ CurrencyConverter/src/currencyconverter/CurrencyConverter.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -16,13 +17,15 @@ import javafx.stage.Stage;
  * @author Janis Tejero
  */
 public class CurrencyConverter extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Calculator.fxml"));
+
+        // icon
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("logo.png")));
         
         Scene scene = new Scene(root);
-        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
@@ -33,5 +36,5 @@ public class CurrencyConverter extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
